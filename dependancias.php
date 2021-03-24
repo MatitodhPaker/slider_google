336f41e5abda4d6cff46b76cf3236ada9604992c
MatitodhPaker <hueco35.perez@gmail.com>
2021-03-24 08:48:50 -0600

    seagrego el index junto con las dependencias las librerias por ultimo el css

---
+++ dependancias.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="librerias/girdder/css/jquery.gridder.min.css">
+<link rel="stylesheet" href="librerias/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/demo.css">
+
+<script src="librerias/jquery-3.3.1.min.js"></script>
+<script src="librerias/bootstrap/js/bootstrap.js"></script>
+<script src="librerias/gridder/js/jquery.gridder.min.js"></script>
