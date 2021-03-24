5314bf6ce6038ac447b25bfe4981db87f76fa3e8
MatitodhPaker <hueco35.perez@gmail.com>
2021-03-24 16:06:59 -0600

    se modifico index.html dependencias.html y se agrego un archivocss

--- dependencias.php
+++ dependencias.php
@@ -1,8 +1,9 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="librerias/girdder/css/jquery.gridder.min.css">
-<link rel="stylesheet" href="librerias/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="librerias/gridder/css/jquery.gridder.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/font-awesome/css/font-awesome.css">
 <link rel="stylesheet" href="css/demo.css">
+<link rel="stylesheet" href="css/main.css">
 
 <script src="librerias/jquery-3.3.1.min.js"></script>
 <script src="librerias/bootstrap/js/bootstrap.js"></script>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title>Google imagenes</title>
   <?php require_once "dependencias.php";?>
   <?php require_once "contenido.php";
-        $datos=contenido();
+      $datos=contenido();
   ?>
 </head>
 <body>
@@ -15,9 +15,45 @@
       <div class="col">
         <h1>Imagenes de google</h1>
         <h2>Animes que he visto</h2>
-        
+        <ul class="gridder-list">
+          <?php
+          for ($i=0; $i < count($datos); $i++):
+            $d=explode("||",$datos[$i]);
+          ?>
+          <li class="gridder-list" data-griddercontent="<?php echo '#gridder-content-'.$i?>">
+            <img src="<?php echo $d[0]?>">
+          </li>
+          <?php
+           endfor;
+          ?>
+        </ul>
       </div>
     </div>
   </div>
 </body>
 </html>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".gridder").gridderExpander({
+			scroll: true,
+			scrollOffset: 60,
+                    scrollTo: "listitem", // panel or listitem
+                    animationSpeed: 100,
+                    animationEasing: "easeInOutExpo",
+                    showNav: true,
+                    nextText: "<i class=\"fa fa-arrow-right\"></i>",
+                    prevText: "<i class=\"fa fa-arrow-left\"></i>",
+                    closeText: "<i class=\"fa fa-times\"></i>",
+                    onStart: function () {
+                    	console.log("Gridder Inititialized");
+                    },
+                    onContent: function () {
+                    	console.log("Gridder Content Loaded");
+                    	$(".carousel").carousel();
+                    },
+                    onClosed: function () {
+                    	console.log("Gridder Closed");
+                    }
+                });
+	});
+</script>
