29fc82f9b2317dcaa82612bda047ff57758a4e22
MatitodhPaker <hueco35.perez@gmail.com>
2021-03-24 11:49:58 -0600

    se modifico contenido.php y se agrego la carpeta img

--- contenido.php
+++ contenido.php
@@ -1,10 +1,15 @@
 <?php
   function contenido(){
     $datos=array();
-    $datos[0]=""."||".""."||"."";
-    $datos[1]=""."||".""."||"."";
+    $datos[0]="img/g3.jpg"."||"."Hataraku Saibou"."||"."Hataraku Saibou trata sobre una historia sobre ti, sobre el interior de tu cuerpo. Según varios estudios, el cuerpo humano posee unos 30 billones de células. Estas células trabajan duro cada día dentro de su mundo, que es tu cuerpo. Desde los Glóbulos Rojos que portan el oxígeno, hasta los Glóbulos Blancos que se enfrentan a las amenazas de las bacterias. ¡Conoce a los héroes y las historias que se desarrollan en tu interior! Basado en el manga de Akane Shimizu, un anime con células antropomorfas y que nos cuenta la vida diaria de nuestro propio cuerpo. ";
+    $datos[1]="img/g4.jpg"."||"."Fire Force"."||"."";
     $datos[2]=""."||".""."||"."";
     $datos[3]=""."||".""."||"."";
     $datos[4]=""."||".""."||"."";
+    $datos[5]=""."||".""."||"."";
+    $datos[6]=""."||".""."||"."";
+    $datos[7]=""."||".""."||"."";
+    $datos[8]=""."||".""."||"."";
+    $datos[9]=""."||".""."||"."";
   }
 ?>
