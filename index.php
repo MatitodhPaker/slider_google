917385d2922667bed82606269b6b2e5374f49a23
MatitodhPaker <hueco35.perez@gmail.com>
2021-03-24 09:09:58 -0600

    se cambie el nombre a denpendencias

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php require_once "dependencias.php"?>
   <title>Document</title>
+  <?php require_once "dependancias.php"?>
 </head>
 <body>
   
